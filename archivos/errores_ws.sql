98073458903384ea7066f063699286ba5fc4353e
desarrolloaba <desarrolloaba@aba-1325a31f853>
2013-08-22 15:04:50 -0500

    Nuevo - Actualizacion 1.1.1 archivos

---
+++ archivos/errores_ws.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE `errores_ws` (
+  `codigo` INTEGER(11) NOT NULL AUTO_INCREMENT,
+  `fecha` DATETIME DEFAULT NULL,
+  `tipo` TINYINT(4) DEFAULT NULL,
+  `modulo` VARCHAR(100) COLLATE utf8_general_ci DEFAULT NULL,
+  `referencia` VARCHAR(50) COLLATE utf8_general_ci DEFAULT NULL,
+  `error` TEXT COLLATE utf8_general_ci,
+  PRIMARY KEY (`codigo`)
+)ENGINE=InnoDB
+AUTO_INCREMENT=72 CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+COMMIT;
+
